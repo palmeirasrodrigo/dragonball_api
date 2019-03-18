e29a2faa0b1c1e9f58f762851ec1dd9683163b5e
Rodrigo <palmeirasrodrigo@hotmail.com>
2019-03-18 18:03:05 -0300

    iisu

--- src/main/java/com/example/dragonball/api/config/property/DragonApiProperty.java
+++ src/main/java/com/example/dragonball/api/config/property/DragonApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("dragonball")
 public class DragonApiProperty {
 	
-	private String originPermitida = "*";
+	private String originPermitida = "http://localhost:8000";
 	
 	private final Seguranca seguranca = new Seguranca();
 	
