ee8b2e61a492d4f18ed0e9d4eac7bec37cfa589f
Rodrigo <palmeirasrodrigo@hotmail.com>
2019-03-19 11:54:49 -0300

    ffd

--- src/main/resources/db/migration/V04__inserir_listas.sql
+++ src/main/resources/db/migration/V04__inserir_listas.sql
@@ -1,1 +1,1 @@
-INSERT INTO usuario (nome, email, senha) values ('Rodrigo Negrão', 'palmeiraselicia@hotmail.com', '$10$bHE/zaRXq9DhZA6P9BWNvOQI2rADIvQzGCKrx3Ji9W0xIMmchr5ZW');
+INSERT INTO usuario (nome, email, senha, pontos) values ('Rodrigo Negrão', 'palmeiraselicia@hotmail.com', '$10$bHE/zaRXq9DhZA6P9BWNvOQI2rADIvQzGCKrx3Ji9W0xIMmchr5ZW', 3);
