7362839893f63d2134d2a2bff8c8e920d409a801
Rodrigo <palmeirasrodrigo@hotmail.com>
2019-03-20 11:12:17 -0300

    jh

--- src/main/java/com/example/dragonball/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/dragonball/api/config/ResourceServerConfig.java
@@ -3,7 +3,6 @@ package com.example.dragonball.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +15,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
-@Profile("oauth-security")
+//@Profile("oauth-security")
+
 @Configuration
 @EnableWebSecurity
 @EnableResourceServer
